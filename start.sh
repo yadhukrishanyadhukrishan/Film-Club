364337faa22a597122a80e892c9274e2a2fbe6e2
Samantha-a <95078987+Samantha-a@users.noreply.github.com>
2022-01-19 20:16:28 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Aadhi000/Ajax.git /Ajax
+  git clone https://github.com/Samantha-a/Film-Club /Film-Club
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Ajax
+  git clone $UPSTREAM_REPO /Film-Club
 fi
-cd /Ajax
+cd /Film-Club
 pip3 install -U -r requirements.txt
-echo "Starting ᗩᒍᗩ᙭....🔥"
+echo "Starting....🔥"
 python3 bot.py
