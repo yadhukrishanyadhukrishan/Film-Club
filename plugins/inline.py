cb86f1ca81ed701029745ea88a542ffbc5ad2448
Samantha-a <95078987+Samantha-a@users.noreply.github.com>
2022-01-26 21:09:00 +0530

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -1,9 +1,8 @@
 import logging
 from pyrogram import Client, emoji, filters
-from pyrogram.errors.exceptions.bad_request_400 import QueryIdInvalid
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, InlineQueryResultCachedDocument
-from database.ia_filterdb import get_search_results
-from utils import is_subscribed, get_size
+
+from utils import get_search_results, is_subscribed, get_size
 from info import CACHE_TIME, AUTH_USERS, AUTH_CHANNEL, CUSTOM_FILE_CAPTION
 
 logger = logging.getLogger(__name__)
@@ -32,7 +31,7 @@ async def answer(bot, query):
 
     offset = int(query.offset or 0)
     reply_markup = get_reply_markup(query=string)
-    files, next_offset, total = await get_search_results(string,
+    files, next_offset = await get_search_results(string,
                                                   file_type=file_type,
                                                   max_results=10,
                                                   offset=offset)
@@ -45,7 +44,7 @@ async def answer(bot, query):
             try:
                 f_caption=CUSTOM_FILE_CAPTION.format(file_name=title, file_size=size, file_caption=f_caption)
             except Exception as e:
-                logger.exception(e)
+                print(e)
                 f_caption=f_caption
         if f_caption is None:
             f_caption = f"{file.file_name}"
@@ -58,9 +57,10 @@ async def answer(bot, query):
                 reply_markup=reply_markup))
 
     if results:
-        switch_pm_text = f"{emoji.FILE_FOLDER} Results - {total}"
+        switch_pm_text = f"{emoji.FILE_FOLDER} Results"
         if string:
             switch_pm_text += f" for {string}"
+
         try:
             await query.answer(results=results,
                            is_personal = True,
@@ -68,8 +68,6 @@ async def answer(bot, query):
                            switch_pm_text=switch_pm_text,
                            switch_pm_parameter="start",
                            next_offset=str(next_offset))
-        except QueryIdInvalid:
-            pass
         except Exception as e:
             logging.exception(str(e))
             await query.answer(results=[], is_personal=True,
@@ -77,6 +75,7 @@ async def answer(bot, query):
                            switch_pm_text=str(e)[:63],
                            switch_pm_parameter="error")
     else:
+
         switch_pm_text = f'{emoji.CROSS_MARK} No results'
         if string:
             switch_pm_text += f' for "{string}"'
@@ -91,6 +90,7 @@ async def answer(bot, query):
 def get_reply_markup(query):
     buttons = [
         [
+            InlineKeyboardButton('JOIИ CHAИИΞL', url='https://t.me/+AMHw_K1wvOM3MTU9'),
             InlineKeyboardButton('🦋 𝗦𝗲𝗮𝗿𝗰𝗵 𝗔𝗴𝗮𝗶𝗻 🦋', switch_inline_query_current_chat=query)
         ]
         ]
