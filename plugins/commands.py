ab5eaa49287cc521d16e93aa4c61be03ad4ccb8e
Samantha-a <95078987+Samantha-a@users.noreply.github.com>
2022-02-01 15:04:41 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -8,7 +8,7 @@ from pyrogram.errors.exceptions.bad_request_400 import ChatAdminRequired
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from database.ia_filterdb import Media, get_file_details, unpack_new_file_id
 from database.users_chats_db import db
-from info import CHANNELS, ADMINS, AUTH_CHANNEL, CUSTOM_FILE_CAPTION, LOG_CHANNEL, PICS
+from info import CHANNELS, ADMINS, AUTH_CHANNEL, LOG_CHANNEL, PICS, CUSTOM_FILE_CAPTION, BATCH_FILE_CAPTION, PROTECT_CONTENT
 from utils import get_size, is_subscribed, temp
 import re
 logger = logging.getLogger(__name__)
