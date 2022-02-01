27e99488d074d3aa0a16dea2a3d71cb6296a2ee4
Samantha-a <95078987+Samantha-a@users.noreply.github.com>
2022-02-01 08:45:01 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1114,17 +1114,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
             text=script.ABOOK_TXT,
             disable_web_page_preview=True,
             reply_markup=reply_markup,
-            parse_mode='html'
-        )
-    elif query.data == "newdata":
-        buttons = [[
-            InlineKeyboardButton('« 𝖡𝖺𝖼𝗄', callback_data='help')
-        ]]
-        reply_markup = InlineKeyboardMarkup(buttons)
-        await query.message.edit_text(
-            text=script.FILE_TXT,
-            reply_markup=reply_markup,
-            parse_mode='html'
+            parse_mode='html'   
         )
     elif query.data == "songs":
         buttons = [[
