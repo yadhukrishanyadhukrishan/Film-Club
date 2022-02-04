299cccd29a5b611e5300188b966e31481c31fcd0
Samantha-a <95078987+Samantha-a@users.noreply.github.com>
2022-02-04 19:03:23 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -700,7 +700,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         await massage.reply_video(
             video="https://te.legra.ph/file/c01ca4adfc54bb88c8bfb.mp4",
             caption="START_TXT.format(query.from_user.mention, temp.U_NAME, temp.B_NAME),"
-            reply_markup=reply_markup,
+            reply_markup=reply_video,
             parse_mode='html'
         )
     elif query.data == "photo":
