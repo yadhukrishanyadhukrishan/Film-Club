5eb2ee3f6b4e4537d538ddae0e167069aef40c0d
Samantha-a <95078987+Samantha-a@users.noreply.github.com>
2022-02-04 18:02:30 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -696,6 +696,12 @@ async def cb_handler(client: Client, query: CallbackQuery):
             reply_markup=reply_markup,
             parse_mode='html'
         )
+        await massage.reply_video
+            video="https://te.legra.ph/file/c01ca4adfc54bb88c8bfb.mp4"
+            caption="START_TXT.format(query.from_user.mention, temp.U_NAME, temp.B_NAME),"
+            reply_markup=reply_markup,
+            parse_mode='html'
+        )
     elif query.data == "photo":
         buttons = [[
             InlineKeyboardButton(text="𝖡𝗋𝗂𝗀𝗍𝗁", callback_data="bright"),
