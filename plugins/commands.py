db0582c6d412b1dcf708e778d6d11af81c753d78
Samantha-a <95078987+Samantha-a@users.noreply.github.com>
2022-02-06 18:37:25 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -70,6 +70,7 @@ async def start(client, message):
         btn = [
             [
                 InlineKeyboardButton(
+                    "🔥 JOIИ CHΛИИΞL 🔥", url=invite_link.invite_link,
                     "🔥 JOIИ CHΛИИΞL 🔥", url=invite_link.invite_link
                 )
             ]
